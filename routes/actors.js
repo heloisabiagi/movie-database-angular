982cf40fb44885629f1546850ddf41f1f250e7be
Heloisa <heloisa.biagi@gmail.com>
2016-01-26 00:30:34 -0200

    Removes unused routes

--- routes/actors.js
+++ routes/actors.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express', myVar: 'Testeee' });
-});
 
 router.get('/ator/cadastrar', function(req, res) {
   res.render('cadastrar-ator', { pageTitle: "Cadastrar Ator"});
